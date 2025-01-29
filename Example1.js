27115ab9d2578c64d1d1737dbb13355fdfb3ebb7
CBasni <149559172+CBasni@users.noreply.github.com>
2025-01-28 22:56:48 -0600

    Added Example 1 for Drawing with P5

---
+++ Example1.js
@@ -0,0 +1,17 @@
+function setup() {
+
+  createCanvas(200, 100);
+  colorMode(HSB);
+
+  background(200);
+
+}
+
+function draw() {
+
+  background(97, 97, 99);
+  circle(50, 50, 80);
+  square(110, 10, 80);
+
+}
+
